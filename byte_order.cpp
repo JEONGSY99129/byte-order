32d9868dbc5522ecb779f754afd5e3ffd9631abb
JEONGSY99129 <39490539+JEONGSY99129@users.noreply.github.com>
2020-07-17 01:46:25 +0900

    Update byte_order.cpp

--- byte_order.cpp
+++ byte_order.cpp
@@ -9,7 +9,7 @@ void byte_order_sum(int n1, int n2){
 	uint32_t fin2=ntohl(num2);
 
 	int dec1=(int)fin1;
-	int dec2-(int)fin2;
+	int dec2=(int)fin2;
 
 	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", dec1, fin1, dec2, fin2, dec1+dec2, fin1+fin2);
 
